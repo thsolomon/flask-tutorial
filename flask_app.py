fe8d2ba555498855b42a56fd1d95d8bd1a033e61
Tiegsti Solomon <tiegsti.solomon@bbc.co.uk>
2019-10-02 12:21:24 +0100

    v2 modified§

--- flask_app.py
+++ flask_app.py
@@ -6,6 +6,10 @@ import pandas as pd
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+@app.route('/')
+def hello():
+	return "Hi there."
+
 @app.route('/api')
 def details_dict():
     return ({ 'username':'thsolomon',
