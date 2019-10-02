dc3a4b06b99906f4b2e445bf6630d3b044497be8
Tiegsti Solomon <tiegsti.solomon@bbc.co.uk>
2019-10-02 11:18:14 +0100

    added api endpoint

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,12 @@
 # flask_app.py
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-
-@app.route("/")
+@app.route('/api')
 def hello():
-    return "Hello World!"
+    return jsonify({'username':'thsolomon',
+                   'email':'tiegsti.solomon@bbc.co.uk',
+                   'id':'WH04M1'})
