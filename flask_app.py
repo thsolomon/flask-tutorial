db9a46471a6eef06d2a2fb4ff403547501b1763e
Tiegsti Solomon <tiegsti.solomon@bbc.co.uk>
2019-10-02 12:01:34 +0100

    api v2

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,20 @@
 # flask_app.py
 from flask import Flask
 from flask import jsonify
+import pandas as pd
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route('/api')
-def hello():
-    return jsonify({'username':'thsolomon',
-                   'email':'tiegsti.solomon@bbc.co.uk',
-                   'id':'WH04M1'})
+def details_dict():
+    return ({ 'username':'thsolomon',
+               'email':'tiegsti.solomon@bbc.co.uk',
+               'id':'WH04M1'})
+
+
+@app.route('/api_v2')
+def details_dict_json():
+    return pd.DataFrame([{ 'username':'thsolomon',
+               'email':'tiegsti.solomon@bbc.co.uk',
+               'id':'WH04M1'}]).to_json(orient='records')
